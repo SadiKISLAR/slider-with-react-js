2476c0b0699a6c6ac9a4d4e3965b5850f27e1be9
SadiKISLAR <sadi22kislar@gmail.com>
2023-01-20 17:30:27 +0300

    index order set

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+  }, [index, people])
   return (
     <section className='section'>
       <div className="title">
@@ -19,6 +26,9 @@ function App() {
           if (personIndex === index) {
             position = 'activeSlide';
           }
+          if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+            position = 'lastSlide'
+          }
 
           return (
             <article className={position} key={id}>
@@ -30,10 +40,10 @@ function App() {
             </article>
           );
         })}
-        <button className='prev'>
+        <button className='prev' onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className='next'>
+        <button className='next' onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
